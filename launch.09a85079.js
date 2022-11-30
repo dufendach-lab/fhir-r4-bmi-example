8a3623c0b029a28080a854841d4e427628645487
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2022-11-30 16:15:15 -0500

    Updates

--- launch.09a85079.js
+++ launch.09a85079.js
@@ -538,7 +538,7 @@ var _fhirclientDefault = parcelHelpers.interopDefault(_fhirclient);
 (0, _fhirclientDefault.default).oauth2.authorize({
     clientId: "my_fhir_web_app",
     fhirServiceUrl: "https://r4.smarthealthit.org",
-    redirectUri: "index.html",
+    redirectUri: "./index.html",
     patientId: "a85b31e5-8f0c-480d-9e81-67408608dd7a"
 });
 
