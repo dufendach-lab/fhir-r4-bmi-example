abe094f5049e3450f87b92802239c8bc05bd1f6f
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2022-11-30 16:29:57 -0500

    update build for relative links

--- src/launch.ts
+++ src/launch.ts
@@ -3,6 +3,6 @@ import FHIR from "fhirclient";
 FHIR.oauth2.authorize({
   clientId: 'my_fhir_web_app',
   fhirServiceUrl: 'https://r4.smarthealthit.org',
-  redirectUri: "index.html",
+  redirectUri: "./index.html",
   patientId: "a85b31e5-8f0c-480d-9e81-67408608dd7a",
 })
