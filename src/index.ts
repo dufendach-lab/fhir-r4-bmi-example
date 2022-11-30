10667cada490dc838d7e9aa5365b7ae98ab356b3
Kevin R. Dufendach, MD, MS <kevin.dufendach@cchmc.org>
2022-11-30 13:40:31 -0500

    Add functions to extract patient name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 import FHIR from "fhirclient";
+import {Patient} from "fhir/r4";
+
+/**
+ * Extract names from FHIR Patient object
+ * @param pt FHIR patient
+ * @return string with list of names or "Name not found"
+ */
+function getPatientName(pt: Patient) {
+  if (pt.name) {
+    const names = pt.name.map(name => `${name.given?.join(" ")} ${name.family}`)
+    return names.join(" / ")
+  }
+
+  return "Name not found"
+}
+
+/**
+ * Update the 'pt_name' document to the text of 'name'
+ *
+ * @param name string containing the patient name(s)
+ */
+function updatePatientName(name: string) {
+  const el = document.getElementById('pt_name');
+
+  if (el) {
+    el.innerHTML = name;
+  }
+}
 
 FHIR.oauth2.ready().then(client => {
   document.getElementById("launch_link")?.remove()
 
-  console.log(client.state.clientId)
+  client.patient.read().then(pt => updatePatientName(getPatientName(pt)))
 })
